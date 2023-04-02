05126a845d3b1a0e4ba3b3bc80b851b7384ce6cc
HorraceMuendo <horracemuendo34@gmail.com>
2023-04-02 20:07:08 +0300

    Added a func to accept a connection

--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
-//get conn
-//accept conn
-//return status
-
 import (
+	//	connectionhandler "connectionhandler/ListeningAndServing"
 	"fmt"
 	"net"
 )
 
-func ListeningAndServing(ln net.Listener, port int) {
+func ListeningAndServing() {
 	var err error
-	ln, err := net.Listen("tcp", ":6060")
+	port := "localhost:5000"
+
+	ln, err := net.Listen("tcp", port)
 	if err != nil {
 		fmt.Println(err)
+	}
+	defer ln.Close()
+
+	host, port, err := net.SplitHostPort(ln.Addr().String())
+
+	fmt.Println("listening on port: %s host: %s \n", port, host)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		go func(conn net.Conn) {
+			buffer := make([]byte, 1024)
+			len, err := conn.Read(buffer)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Printf("Message recieved: %s\n ", string(buffer[:len]))
+			conn.Write([]byte("message recieved .\n"))
+			conn.Close()
+		}(conn)
 	}
 
 }
 
+func AcceptDial() {
+	address := ""
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Unable to establish a connection", err)
+	}
+	fmt.Println("connection established on address: %s \n", conn)
+}
+
 func main() {
+	//connectionhandler.ListeningAndServing()
+	ListeningAndServing()
+	AcceptDial()
 	fmt.Println("i am listening.....")
 }
